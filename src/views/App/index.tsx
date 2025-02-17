44961fae851d41cfd3d6623c4ef06922edbef05c
Abdulazimov Ruslan <abdulazimov.03@mail.ru>
2025-02-17 12:54:35 +0500

    start project

---
+++ src/views/App/index.tsx
@@ -0,0 +1,14 @@
+import React, { FC } from "react";
+
+import styles from "./index.module.scss";
+
+export const App: FC = () => {
+    return (
+        <article className={styles.article}>
+            <h1 className={styles.articleTitle}>To Do App</h1>
+            <section className="styles.articleSection">
+
+            </section>
+        </article>
+    )
+}
